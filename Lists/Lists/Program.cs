30d299ef085b4fb1136d8322a39d5638985decd0
Tamer Alssaleh (Paradoxial) <52636947+Paradoxial8172@users.noreply.github.com>
2024-04-16 12:10:45 -0500

    Add files via upload

---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<dynamic> myList = new List<dynamic> ();
+            myList.Add(3);
+            myList.Add("Hello");
+            myList.Add(3.14159265);
+
+            Console.WriteLine(myList[0]); //element 1
+            Console.WriteLine(myList[1]); //element 2
+            Console.WriteLine(myList[2]); //element 3
+        }
+    }
+}
